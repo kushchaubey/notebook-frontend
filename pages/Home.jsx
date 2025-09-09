30d358fa55da80ca482a0c9c0cf1b6ca9079a3ed
Vikas <vckushworld270@gmail.com>
2025-09-09 16:12:30 +0530

    model created

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -3,6 +3,9 @@ import NoteTable from '../src/components/NoteTable';
 import AddNote from '../src/components/AddNote';
 import SearchField from '../src/components/SearchField';
 import { useState } from 'react';
+import Model from '../src/components/Model';
+import InputFormGroup from "../src/components/formsComponent/InputFormGroup";
+import FormButton from "../src/components/formsComponent/formButton";
 const columns = [
 	{
 		name: 'Title',
@@ -48,14 +51,16 @@ const datas = [
 
 
 const Home = ()=>{
-
+     
      const [filteredData, setFilteredData]   = useState(datas);
      const [searchTerm, setSearchTerm]   = useState('')
+    const [isModelOpen, setIsModelOpen]  = useState(false);
+
 
   function filterData(event){
         setSearchTerm(event.target.value)
         const filterData =datas.filter((data)=>{
-                return data.title.toLowerCase().includes(event.target.value)
+                return data.title.toLowerCase().includes(event.target.value.toLowerCase())
             })
         setFilteredData(filterData)
   }
@@ -64,14 +69,23 @@ const Home = ()=>{
     return(
          <>
          <div className="box-container max-w-[1000px]" >
+           
                   <h1 className="heading">Notes</h1>
 
             <div className='flex justify-between mb-4 mt-4'>
-                <AddNote label="Add Note"/>
+                <AddNote label="Add Note" className="cursor-pointer" onClick={()=>{setIsModelOpen(true)}}/>
                <SearchField placeholder="Search notes" onChange={(e)=>{filterData(e)}} value={searchTerm} />
             </div>    
                 
                 <NoteTable filteredData={filteredData} cols={columns} paginationValid={true}/>
+
+                 <Model heading="Form 16" openModel={isModelOpen} closeModel={setIsModelOpen}> <form action="">
+                     <InputFormGroup label="UserName" id="userName" name="userName" type="text" value=''/>
+                     <InputFormGroup label="password" id="password" name="password" type="password" value=''/>
+                     <FormButton buttonname="Login"/>
+                   </form>
+
+            </Model >
  
          </div>
 
--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -1,6 +1,22 @@
-const Model = ()=>{
-    return(
 
+const Model = ({children, heading, openModel,closeModel})=>{
+
+           
+    return(
+     <div className={`w-full h-full fixed bg-black/60 z-50 top-0 left-0 flex justify-center items-center ${openModel ? "opacity-100" : "opacity-0 pointer-events-none"}`} onClick={()=>{
+                        closeModel(false);}}>
+          <div className={`max-w-[700px] min-h-[200px] min-w-[200px] sm:w-[400px] md:w-[700px]   px-6 py-7 bg-background rounded-2xl m-auto relative transition-transform duration-200 ease-in 
+          ${openModel ? "translate-y-0" : "-translate-y-full"} ` }    onClick={(e) => e.stopPropagation()}>
+                    <div className="absolute right-3 top-3 font-bold text-text text-2xl cursor-pointer" onClick={()=>{
+                        closeModel(false);
+                    }}>X</div>
+                     <h1 className="text-text text-2xl font-bold text-center mb-4">{heading}</h1>
+                    <div className="">
+                        {children}
+                    </div>
+       </div>
+     </div>
+     
     )
 }
 
